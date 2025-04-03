42b628535650f59e3397a048dd03311378e6da0d
Vineet Panwar <vineet@nextlevelflag.com>
2025-04-03 02:18:26 -0400

    feat: testing Github Action

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Hello from ProCode on GCP! Hope you are doing great...");
+  res.send("🚀 Hello from ProCode on GCP! Hope you are doing great... Just checking if GCP cloud run is working fine ");
 });
 
 app.listen(PORT, () => {
